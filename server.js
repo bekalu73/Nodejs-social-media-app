861e553c5956a118f1d8eeca5c8965c893e62018
bekalu73 <bekalusisay2010@gmail.com>
2024-05-28 17:43:18 +0300

    add post route and controller

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/users.js");
 const authRoute = require("./routes/auth.js");
+const postRoute = require("./routes/posts");
 
 const app = express();
 dotenv.config();
@@ -26,6 +27,7 @@ app.use(morgan("common"));
 //routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
   console.log("backend server is Ready @ http://localhost:8800");
